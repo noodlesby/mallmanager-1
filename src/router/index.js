e767307a9beee127a82ad350cbb7fb21fa0cbbdf
noodles <wzp23@126.com>
2018-11-26 19:57:35 +0800

    登陆页面布局

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Login from '@/views/login/login.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-
+    {
+      name: 'login',
+      path: '/login',
+      component: Login
+    }
   ]
 })
---
+++ src/views/login/login.vue
@@ -0,0 +1,29 @@
+<template>
+    <el-form label-position="top" label-width="80px" :model="formdata">
+      <h2>用户登录</h2>
+  <el-form-item label="用户名">
+    <el-input v-model="formdata.username"></el-input>
+  </el-form-item>
+  <el-form-item label="密码">
+    <el-input v-model="formdata.password"></el-input>
+  </el-form-item>
+  <el-button type="primary">登陆</el-button>
+</el-form>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      formdata: {
+        username: '',
+        password: ''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
